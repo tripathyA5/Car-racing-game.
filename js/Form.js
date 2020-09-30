a28eb161256d15352064a9b5a55e12db2f5c4483
tripathyA5 <69359952+tripathyA5@users.noreply.github.com>
2020-09-29 21:49:41 -0400

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -37,7 +37,8 @@ class Form {
     this.reset.mousePressed(()=>{
       player.updateCount(0);
       game.update(0);
-      player.deletePlayers();
+      Player.deletePlayers();
+      Player.updateCarsAtEnd(0);
     });
 
   }
